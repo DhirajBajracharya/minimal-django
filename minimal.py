91815616a2fd2606fbcd2228d58a21480696a283
dj <dj@dj.com>
2020-04-27 20:35:12 +0545

    More urls , sending parameters regex!

--- minimal.py
+++ minimal.py
@@ -17,9 +17,16 @@ def index(request):
 def home(request):
     return HttpResponse('Welcome to the Tinyapp\'s Homepage!')
 
+def gettry(request):
+    color = request.GET.get('color', '')#http://127.0.0.1:8005/tryget?color=blue
+    return HttpResponse(
+        '<h1 style="color:' + color + '">Welcome to the Tinyapp\'s Homepage!</h1>'
+    )  # don't use user input like that in real projects!
+
 urlpatterns = [
     url(r'^$', index),
-    url(r'home^$', home),
+    url(r'^home$', home),
+    url(r'^tryget$', gettry),
 ]
 
 if __name__ == '__main__':
